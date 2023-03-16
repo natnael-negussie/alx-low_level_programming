c1b16a37affe3eb21513bc7038ca9112845b0ee6
Natnael <natnael_negussie@yahoo.com>
2023-03-16 13:33:08 -0700

    puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -3,13 +3,13 @@
 /**
  * main - Entry point
  *
- * Description: A C program that prints with puts function
+ * Description: A C program that prints with put function
  *
  * Return: Always 0 (Success)
 */
 
 int main(void)
 {
-            puts ("\"Programming is like building a multilingual puzzle");
-            return (0);
+            puts("\"Programming is like building a multilingual puzzle");
+            return(0);
 }
